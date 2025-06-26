69c47424125bca42421af2aa9c65f11bc320faaf
DevdScvn <nikita@MacBook-Air-Nikita.local>
2025-06-26 21:28:29 +0300

    init

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "db_helper",
     "Base",
+    "Prediction",
+    "Team",
+    "Match",
 )
 
 from database.base import Base
 from database.db_helper import db_helper
+from football.models import Prediction, Team, Match
